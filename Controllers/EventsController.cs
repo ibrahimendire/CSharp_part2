503d6a3325dc2c213c1a51099a7ee8995f157f85
ibrahimendire <ibrahimendire@users.noreply.github.com>
2020-12-05 21:59:07 -0600

    Event Model added

--- Controllers/EventsController.cs
+++ Controllers/EventsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HelloASPDotNET.Models;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -10,8 +11,8 @@ namespace HelloASPDotNET.Controllers
 {
     public class EventsController : Controller
     {
-        //static private List<string> Events = new List<string>();
-        static private Dictionary<string, string> Events = new Dictionary<string, string>();
+        static private List<Event> Events = new List<Event>();
+        //static private Dictionary<string, string> Events = new Dictionary<string, string>();
         // GET: /<controller>/
         [HttpGet]
         public IActionResult Index()
@@ -30,9 +31,10 @@ namespace HelloASPDotNET.Controllers
 
         [HttpPost]
         [Route("/Events/Add")]
-        public IActionResult NewEvent(String name ,string description="")
+        public IActionResult NewEvent(String name ,string desc)
         {
-            Events.Add(name,description);
+
+            Events.Add(new Event(name ,desc));
             return Redirect("/Events"); 
         }
     }
---
+++ Models/Event.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace HelloASPDotNET.Models
+{
+    public class Event
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; }
+        public int Id { get; }
+        static private int nextId = 1;
+
+        public Event(string name ,string desc)
+        {
+            Name = name;
+            Description = desc;
+            Date = DateTime.Now;
+            Id = nextId;
+            nextId++;
+        }
+
+
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Event @event &&
+                   Id == @event.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
+    }
+}
+
